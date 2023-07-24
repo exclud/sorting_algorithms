8c1b37e42b1a0c7365cb54aaf8808b5f5f0c93a9
exclud <shammahnjomo12@gmail.com>
2023-07-24 21:20:22 +0300

    Function to sort a doubly linked list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,1 +1,43 @@
 #include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       in ascending order using Insertion Sort.
+ * @list: Pointer to the head of the doubly linked list.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *sorted, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	sorted = (*list)->next;
+
+	while (sorted != NULL)
+	{
+		current = sorted;
+		sorted = sorted->next;
+
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			/* Swap the nodes */
+			temp = current->prev;
+			current->prev = temp->prev;
+			temp->prev = current;
+			temp->next = current->next;
+			current->next = temp;
+
+			if (temp->next != NULL)
+				temp->next->prev = temp;
+
+			if (current->prev != NULL)
+				current->prev->next = current;
+
+			if (current->prev == NULL)
+				*list = current;
+
+			print_list(*list);
+		}
+	}
+}
