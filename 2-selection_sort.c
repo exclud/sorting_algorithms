580b08e07757ed3c224d73ced992ae520ae717b0
exclud <shammahnjomo12@gmail.com>
2023-07-25 20:05:44 +0300

    Betty Compliance

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,39 +1,39 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers in ascending order using Selection Sort
+ * selection_sort - Sorts an array of integers in ascending order
  *
  * @array: The array to be sorted
  * @size: Number of elements in the array
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_index;
-    int temp;
+	size_t i, j, min_index;
+	int temp;
 
-    if (array == NULL || size <= 1)
-        return;
+	if (array == NULL || size <= 1)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_index = i;
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
 
-        for (j = i + 1; j < size; j++)
-        {
-            /* Find the index of the minimum element in the unsorted part of the array */
-            if (array[j] < array[min_index])
-                min_index = j;
-        }
+		for (j = i + 1; j < size; j++)
+		{
+			/* Find the index of the minimum element in the unsorted part*/
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
 
-        if (min_index != i)
-        {
-            /* Swap the elements */
-            temp = array[i];
-            array[i] = array[min_index];
-            array[min_index] = temp;
+		if (min_index != i)
+		{
+			/* Swap the elements */
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
 
-            /* Print the array after the swap */
-            print_array(array, size);
-        }
-    }
+			/* Print the array after the swap */
+			print_array(array, size);
+		}
+	}
 }
