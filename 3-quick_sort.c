44d93029841a77bc602e76674e18b65b79219b84
exclud <shammahnjomo12@gmail.com>
2023-07-25 20:14:24 +0300

    Fix: Recursive Quick Sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,10 +9,10 @@
 void quick_sort(int *array, size_t size);
 
 /* Helper function to perform the Lomuto partition */
-size_t lomuto_partition(int *array, size_t low, size_t high, size_t size);
+size_t lomuto_partition(int *array, size_t lo, size_t hi, size_t size);
 
-/* Helper function to perform the actual Quick Sort */
-void quick_sort_recursive(int *array, size_t low, size_t high, size_t size);
+/* Helper function to perform the actual quick sort recursively */
+void quick_sort_recursive(int *array, size_t lo, size_t hi, size_t size);
 
 /**
  * quick_sort - Sorts an array of integers in ascending order using Quick Sort
@@ -22,75 +22,76 @@ void quick_sort_recursive(int *array, size_t low, size_t high, size_t size);
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size <= 1)
-        return;
+	if (array == NULL || size <= 1)
+		return;
 
-    quick_sort_recursive(array, 0, size - 1, size);
+	quick_sort_recursive(array, 0, size - 1, size);
 }
 
 /**
  * lomuto_partition - Lomuto partition scheme for Quick Sort
  *
- * @array: The array to be sorted
- * @low: Starting index of the partition
- * @high: Ending index of the partition
+ * @array: The array to be partitioned
+ * @lo: Starting index of the partition
+ * @hi: Ending index of the partition
  * @size: Number of elements in the array
  *
- * Return: The final position of the pivot after partitioning
+ * Return: Index of the pivot element after partitioning
  */
-size_t lomuto_partition(int *array, size_t low, size_t high, size_t size)
+size_t lomuto_partition(int *array, size_t lo, size_t hi, size_t size)
 {
-    int pivot, temp;
-    size_t i, j;
+	int pivot, temp;
+	size_t i, j;
 
-    pivot = array[high];
-    i = low - 1;
+	pivot = array[hi];
+	i = lo;
 
-    for (j = low; j < high; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            i++;
+	for (j = lo; j < hi; j++)
+	{
+		if (array[j] < pivot)
+		{
+			/* Swap elements at i and j */
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
 
-            /* Swap the elements */
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+			/* Print the array after the swap */
+			print_array(array, size);
 
-            /* Print the array after the swap */
-            print_array(array, size);
-        }
-    }
+			i++;
+		}
+	}
 
-    /* Swap the pivot with the element at position i + 1 */
-    temp = array[i + 1];
-    array[i + 1] = array[high];
-    array[high] = temp;
+	/* Swap the pivot element to its correct position */
+	temp = array[i];
+	array[i] = array[hi];
+	array[hi] = temp;
 
-    /* Print the array after the swap */
-    print_array(array, size);
+	/* Print the array after the swap */
+	print_array(array, size);
 
-    return i + 1;
+	return (i);
 }
 
 /**
  * quick_sort_recursive - Recursive function to perform Quick Sort
  *
  * @array: The array to be sorted
- * @low: Starting index of the partition
- * @high: Ending index of the partition
+ * @lo: Starting index of the partition
+ * @hi: Ending index of the partition
  * @size: Number of elements in the array
  */
-void quick_sort_recursive(int *array, size_t low, size_t high, size_t size)
+void quick_sort_recursive(int *array, size_t lo, size_t hi, size_t size)
 {
-    if (low < high)
-    {
-        /* Perform the partition and get the pivot position */
-        size_t pivot = lomuto_partition(array, low, high, size);
+	size_t pivot;
 
-        /* Recursively sort the left and right partitions */
-        if (pivot > 0)
-            quick_sort_recursive(array, low, pivot - 1, size);
-        quick_sort_recursive(array, pivot + 1, high, size);
-    }
+	if (lo < hi)
+	{
+		pivot = lomuto_partition(array, lo, hi, size);
+
+		if (pivot > 0)
+			quick_sort_recursive(array, lo, pivot - 1, size);
+
+		quick_sort_recursive(array, pivot + 1, hi, size);
+	}
 }
--- sort.h
+++ sort.h
@@ -28,6 +28,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 listint_t *create_listint(const int *array, size_t size);
+void quick_sort_recursive(int *array, size_t lo, size_t hi, size_t size);
+
 
 #endif /* SORT_H */
 
